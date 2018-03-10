fcdb30fce3c415a09fa8d8e5111b75a3d4a2e7a9
Emre Filiz <e_filiz@fanshaweonline.ca>
2018-03-10 05:51:07 +0300

    Added detail screen of movies

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,18 +2,99 @@
   var movieThumbnailsDiv = document.querySelector("#movie-thumbnail");
   var artistThumbnailsDiv = document.querySelector("#artist-thumbnail");
 
-  function getMovieCovers() {
+  function getMoviePosters() {
     let url = "movie/getMovies";
     fetch(url)
       .then(resp => resp.json())
       .then(data => {
-        let movieWrapper = movieThumbnailsDiv.querySelector(
-          ".thumbnail-list> ul"
-        );
+        let movieWrapper = movieThumbnailsDiv.querySelector(".thumbnail-list");
         data.movies.forEach(({ movies_id, movies_title, movies_cover }) => {
-          let movieThumbnail = `<img data-id=${movies_id} src="images/${movies_cover}" alt="${movies_title} cover" class="movie-thumbnail">`;
+          let movieThumbnail = `<div class="card"><img data-id=${movies_id} data-toggle="modal" data-target="#movieModal" src="images/${movies_cover}" alt="${movies_title} cover" class="card-img-top movie-thumbnail"><div class="card-body">
+          <h5 class="card-title">${movies_title}</h5></div></div>`;
           movieWrapper.innerHTML += movieThumbnail;
         });
+        let movies = movieWrapper.querySelectorAll(".movie-thumbnail");
+        movies.forEach(movie => {
+          movie.addEventListener("click", getMovieDetails, false);
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  //
+  function getMovieDetails(evt) {
+    let id = evt.currentTarget.dataset.id;
+    getMovieInformation(id);
+    getMovieGenre(id);
+    getParentalRatingofMovie(id);
+  }
+
+  //
+  function getMovieInformation(movieId) {
+    let url = "movie/getMovie/" + movieId;
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let movieModal = document.querySelector("#movieModal");
+        let movieTitle = movieModal.querySelector("#movieModalLabel");
+        let movieYear = movieModal.querySelector("#movieYear");
+        let movieCover = movieModal.querySelector("#movieCover");
+        let movieRating = movieModal.querySelector("#movieRating");
+        let movieStoryline = movieModal.querySelector("#movieStoryline");
+
+        data.movie.forEach(
+          ({
+            movies_title,
+            movies_year,
+            movies_storyline,
+            movies_rating,
+            movies_cover
+          }) => {
+            movieTitle.innerHTML = movies_title;
+            movieCover.src = `images/${movies_cover}`;
+            movieCover.alt = movies_title;
+            movieYear.innerHTML = movies_year;
+            movieStoryline.innerHTML = movies_storyline;
+            movieRating.innerHTML = "Rating: " + movies_rating;
+          }
+        );
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  function getMovieGenre(movieId) {
+    let url = "movie/getGenre/" + movieId;
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let movieGenre = movieModal.querySelector("#movieGenre");
+        let genres = "";
+        data.genre.forEach(({ genre_name }) => {
+          genres += `${genre_name}, `;
+        });
+        genres = genres.replace(/, $/, "");
+        movieGenre.innerHTML = genres;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  function getParentalRatingofMovie(movieId) {
+    let url = "movie/getAgeRating/" + movieId;
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let moviePG = movieModal.querySelector("#moviePG");
+        let rating = '';
+        data.agerating.forEach(({ age_rating_name }) => {
+          rating = `${age_rating_name}`;
+        });
+        moviePG.innerHTML = rating;
       })
       .catch(function(error) {
         console.log(error);
@@ -26,10 +107,11 @@
       .then(resp => resp.json())
       .then(data => {
         let artistWrapper = artistThumbnailsDiv.querySelector(
-          ".thumbnail-list > ul"
+          ".thumbnail-list"
         );
         data.movies.forEach(({ artist_id, artist_name, artist_photo }) => {
-          let artistThumbnail = `<img data-id=${artist_id} src="images/${artist_photo}" alt="${artist_name} cover" class="artist-thumbnail">`;
+          let artistThumbnail = `<div class="card"><img data-id=${artist_id} src="images/${artist_photo}" alt="${artist_name} cover" class="card-img-top artist-thumbnail"><div class="card-body">
+          <h5 class="card-title">${artist_name}</h5></div></div>`;
           artistWrapper.innerHTML += artistThumbnail;
         });
       })
@@ -38,6 +120,6 @@
       });
   }
 
-  getMovieCovers.call();
+  getMoviePosters.call();
   getArtistPhotos.call();
 })();
--- routes/movie.js
+++ routes/movie.js
@@ -26,4 +26,55 @@ router.get("/getMovies", (req, res) => {
   });
 });
 
+// Get Detailed Information of a Movie by Id
+router.get("/getMovie/:id", (req, res) => {
+  let getMovie = `SELECT * FROM tbl_movies WHERE movies_id = ${req.params.id}`;
+  connect.query(getMovie, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.json({
+        movie: result
+      });
+    }
+  });
+});
+
+// Get Genres of a Movie by Id
+router.get("/getGenre/:id", (req, res) => {
+  let getGenre = `SELECT g.* FROM tbl_movies m, tbl_mov_gen mg, tbl_genre g WHERE m.movies_id = ${
+    req.params.id
+  } AND m.movies_id = mg.movies_id AND mg.genre_id = g.genre_id`;
+  connect.query(getGenre, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.json({
+        genre: result
+      });
+    }
+  });
+});
+
+// Get Age Rating of a Movie by Id
+router.get("/getAgeRating/:id", (req, res) => {
+  let getAgeRating = `SELECT r.* FROM tbl_movies m, tbl_mov_ara ma, tbl_age_rating r WHERE m.movies_id = ${
+    req.params.id
+  } AND m.movies_id = ma.movies_id AND ma.age_rating_id = r.age_rating_id`;
+  connect.query(getAgeRating, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.json({
+        agerating: result
+      });
+    }
+  });
+});
 module.exports = router;
